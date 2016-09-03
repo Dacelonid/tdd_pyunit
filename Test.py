026bbac51201409b4208dc3e61489cac7b344461
Ken <kenneth.oneill@gmail.com>
2016-09-03 22:10:48 +0100

    Step 3 Dynamically invoking test_method

--- Test.py
+++ Test.py
@@ -1,14 +1,16 @@
 class WasRun():
     def __init__(self, name):
         self.was_run = None
+        self.name = name
 
     def run(self):
-        self.test_method()
+        method = getattr(self, self.name)
+        method()
 
     def test_method(self):
         self.was_run = True
 
-test = WasRun("testMethod")
+test = WasRun("test_method")
 print (test.was_run)
 test.run()
 print (test.was_run)
