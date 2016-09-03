5d46b498ffc6770d84fce23c8ae3d455d03f7214
Ken <kenneth.oneill@gmail.com>
2016-09-03 22:21:00 +0100

    Replacing print with assert

--- Test.py
+++ Test.py
@@ -1,16 +1,26 @@
-class WasRun():
+class TestCase:
     def __init__(self, name):
-        self.was_run = None
         self.name = name
 
     def run(self):
         method = getattr(self, self.name)
         method()
 
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = None
+        TestCase.__init__(self, name)
+
     def test_method(self):
         self.was_run = True
 
-test = WasRun("test_method")
-print (test.was_run)
-test.run()
-print (test.was_run)
+class TestCaseTest(TestCase):
+    def test_running_test(self):
+        test = WasRun("test_method")
+        assert(not test.was_run)
+        test.run()
+        assert(test.was_run)
+
+TestCaseTest("test_running_test").run()
